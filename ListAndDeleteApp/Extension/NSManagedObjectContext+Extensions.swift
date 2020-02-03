1c493b9af0b7be9c3f117226ad885bb502be4e92
Toni <toni.tani@student.saimia.fi>
2020-02-03 13:07:46 +0200

    Extensions Library for object management

---
+++ ListAndDeleteApp/Extension/NSManagedObjectContext+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  NSManagedObjectContext+Extensions.swift
+//  ListAndDeleteApp
+//
+//  Created by Toni Tani on 3.2.2020.
+//  Copyright © 2020 Toni Tani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+extension NSManagedObjectContext {
+    
+    static var current: NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+}
